71752f038023cad5e0641cd4f542c56fb2e4e515
Abderrahim TOUZOURT <a.touzourt@gmail.com>
2025-07-16 15:40:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// autoloading classes
+require_once __DIR__.'/vendor/autoload.php';
+
+
+use App\Database\Database as DB;
+
+$con = new DB();
+
+$con->connect();
+
---
+++ src/App/Database/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $servername = '127.0.0.1';
+    private $username = 'root';
+    private $password = 'root';
+    private $dbname = 'php_react_db';
+    private $port = 8889;
+
+    public function connect()
+    {
+        try {
+            $db = new PDO("mysql:host=". $this->servername .";port=". $this->port .";dbname=" .$this->dbname,
+                        $this->username,
+                        $this->password
+                    );
+            // $db = new PDO(
+            //     "mysql:host=127.0.0.1;port=8889;dbname=php_react_db",
+            //     $this->username,
+            //     $this->password
+            // );
+
+            $db->exec("set names utf8");
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $db;
+        } catch (PDOException $e) {
+            echo "Connection failed ". $e->getMessage();
+        }
+    }
+
+}
